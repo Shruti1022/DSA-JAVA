42a12e66939287771916ddcc9155f1e4a1cb8f3e
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-09-16 16:18:23 +0530

    Time: 1 ms (99.77%) | Memory: 42.1 MB (81.72%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,28 +1,29 @@
 class Solution {
     public String addBinary(String a, String b) {
-    int n1 = a.length() -1;
-    int n2 = b.length() -1;
-    int c = 0, base=2;
-    StringBuilder res=new StringBuilder();
-    while(n1>=0 || n2>=0){
-        int t1 = 0, t2 = 0, sum;
-           if(n1>=0)
-           t1 = a.charAt(n1--)-'0';
-           if(n2>=0)
-           t2 = b.charAt(n2--)-'0';
-           sum = t1 + t2 + c;
+        StringBuilder sb = new StringBuilder();
 
-           if(sum>=base){
-            c=1;
-            sum-=base;
-           }
-           else
-           c=0;
-           res.append(sum);
-    }    
-    if(c==1)
-    res.append(c);
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-    return res.reverse().toString();
+        // iterate from back of both strings
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0'; // convert char to int
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            sb.append(sum % 2);   // add binary digit
+            carry = sum / 2;      // update carry
+        }
+
+        return sb.reverse().toString();
     }
 }
