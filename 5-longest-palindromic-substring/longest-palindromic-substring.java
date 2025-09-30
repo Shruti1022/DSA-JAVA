e63f4860c5fb4b0b96cc039c42c51dc45cf9d2e2
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-09-30 14:12:28 +0530

    Time: 19 ms (49.12%) | Memory: 45.3 MB (35.87%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,51 @@
 class Solution {
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
+    //     int start = 0, end = 0;
+
+    //     for (int i = 0; i < s.length(); i++) {
+    //         int len1 = expandFromCenter(s, i, i);      
+    //         int len2 = expandFromCenter(s, i, i + 1);  
+    //         int len = Math.max(len1, len2);
+
+    //         if (len > end - start) {
+    //             start = i - (len - 1) / 2;
+    //             end = i + len / 2;
+    //         }
+    //     }
+    //     return s.substring(start, end + 1);
+    // }
+
+    // private int expandFromCenter(String s, int left, int right) {
+    //     while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    //         left--;
+    //         right++;
+    //     }
+    //     return right - left - 1;
+
+   String LPS = "";
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandFromCenter(s, i, i);      
-            int len2 = expandFromCenter(s, i, i + 1);  
-            int len = Math.max(len1, len2);
+           
+            String odd = expandFromCenter(s, i, i);
+          
+            String even = expandFromCenter(s, i, i + 1);
 
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+            String curr = odd.length() > even.length() ? odd : even;
+
+            if (curr.length() > LPS.length()) {
+                LPS = curr;
             }
         }
-        return s.substring(start, end + 1);
+        return LPS;
     }
 
-    private int expandFromCenter(String s, int left, int right) {
+    private String expandFromCenter(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return right - left - 1;
+
+        return s.substring(left + 1, right);
     }
-}
+    }
