6762ce880ebf7dac64dafc1489c67a1100eb5f42
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-10-23 19:14:36 +0530

    Time: 9 ms (42.94%) | Memory: 46.6 MB (57.13%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,28 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
         if (intervals.length <= 1) return intervals;
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        List<int[]> merged = new ArrayList<>();
+        Stack<int[]> stack = new Stack<>();
 
-        int[] current = intervals[0];
+        stack.push(intervals[0]);
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] next = intervals[i];
+            int[] top = stack.peek();
+            int[] current = intervals[i];
 
-            if (next[0] <= current[1]) {
-                current[1] = Math.max(current[1], next[1]); 
+            if (current[0] <= top[1]) {
+                top[1] = Math.max(top[1], current[1]);  
+                stack.pop();
+                stack.push(top);
             } else {
-                merged.add(current); 
-                current = next;      
+                stack.push(current);
             }
         }
 
-        merged.add(current);
+        int[][] result = new int[stack.size()][2];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
 
-        return merged.toArray(new int[merged.size()][]);
+        return result;
     }
 }
