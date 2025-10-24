467dd34de99fe6a5aab17a33025e887a3565c79e
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-10-24 20:23:09 +0530

    Time: 8 ms (84.97%) | Memory: 46.9 MB (16.17%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,28 +4,17 @@ class Solution {
 
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        Stack<int[]> stack = new Stack<>();
-
-        stack.push(intervals[0]);
+        int idx = 0; 
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] top = stack.peek();
-            int[] current = intervals[i];
-
-            if (current[0] <= top[1]) {
-                top[1] = Math.max(top[1], current[1]);  
-                stack.pop();
-                stack.push(top);
+            if (intervals[i][0] <= intervals[idx][1]) {
+                intervals[idx][1] = Math.max(intervals[idx][1], intervals[i][1]);
             } else {
-                stack.push(current);
-            }
+                idx++;
+                intervals[idx] = intervals[i];
+                     }
         }
 
-        int[][] result = new int[stack.size()][2];
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-
-        return result;
+        return Arrays.copyOfRange(intervals, 0, idx + 1);
     }
 }
