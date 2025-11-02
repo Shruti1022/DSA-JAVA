7004d31046d3aabc9a41f7a7dc4a84f39e2b0e3e
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-11-02 18:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (5.14%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -9,7 +9,7 @@ class Solution {
                 mid++;
             } else if (nums[mid] == 1) {
                 mid++;
-            } else { // nums[mid] == 2
+            } else { 
                 swap(nums, mid, high);
                 high--;
             }
