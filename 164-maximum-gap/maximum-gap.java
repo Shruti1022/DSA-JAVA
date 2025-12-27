440e90a71d7b2b35ba1f0433423de0f245c14d9e
Shruti Vishwakarma <116656214+Shruti1022@users.noreply.github.com>
2025-12-27 14:27:44 +0530

    Time: 46 ms (34.27%) | Memory: 91.3 MB (10.76%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,49 +1,16 @@
 class Solution {
     public int maximumGap(int[] nums) {
-         if (nums == null || nums.length < 2) {
+        if(nums == null || nums.length < 2){
             return 0;
         }
+        
+        Arrays.sort(nums);
 
-        int n = nums.length;
-        int minVal = nums[0], maxVal = nums[0];
+        int maximum = nums[1] - nums[0];
 
-        for (int num : nums) {
-            minVal = Math.min(minVal, num);
-            maxVal = Math.max(maxVal, num);
+        for(int i = 2; i<nums.length; i++){
+            maximum = Math.max(maximum, nums[i] - nums[i-1]);
         }
-
-        if (minVal == maxVal) {
-            return 0;
-        }
-
-        int bucketSize = Math.max(1, (maxVal - minVal) / (n - 1));
-        int bucketCount = (maxVal - minVal) / bucketSize + 1;
-
-        int[] bucketMin = new int[bucketCount];
-        int[] bucketMax = new int[bucketCount];
-
-        for (int i = 0; i < bucketCount; i++) {
-            bucketMin[i] = Integer.MAX_VALUE;
-            bucketMax[i] = Integer.MIN_VALUE;
-        }
-
-        for (int num : nums) {
-            int idx = (num - minVal) / bucketSize;
-            bucketMin[idx] = Math.min(bucketMin[idx], num);
-            bucketMax[idx] = Math.max(bucketMax[idx], num);
-        }
-
-        int maxGap = 0;
-        int prevMax = minVal;
-
-        for (int i = 0; i < bucketCount; i++) {
-            if (bucketMin[i] == Integer.MAX_VALUE) {
-                continue; 
-            }
-            maxGap = Math.max(maxGap, bucketMin[i] - prevMax);
-            prevMax = bucketMax[i];
-        }
-
-        return maxGap;
+        return maximum;
     }
 }
